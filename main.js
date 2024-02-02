533484992f572e0ff38b5ba507be18900cc7fc4d
Heather-Angela1 <124858135+Heather-Angela1@users.noreply.github.com>
2024-02-02 01:38:29 -0800

    Scroll Button fix

--- main.js
+++ main.js
@@ -29,6 +29,6 @@ document.addEventListener("click", function (event) {
 $(function() {
   $('a[href*=#]').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'swing');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
   });
 });
