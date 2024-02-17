db736b4124748a5d7812908d88ef340c9f765e71
Heather-Angela1 <124858135+Heather-Angela1@users.noreply.github.com>
2024-02-17 13:11:04 -0800

    debug scroll down button

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ document.addEventListener("click", function (event) {
 
 // Scroll down button
 $(function() {
-  $('a[href*=#]').on('click', function(e) {
+  $('a[href*=\\#]').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
   });
